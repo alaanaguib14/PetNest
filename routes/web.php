4ccf1c49453283456be9b735fb3ded0327e33687
alaanaguib14 <alaanaguib96@gmail.com>
2026-03-03 00:46:24 +0200

    update web

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json('Welcome to PetNest');
+    return response()->json('Welcome to PetNest to start get to Register or login via https://petnest-production.up.railway.app/api/register or https://petnest-production.up.railway.app/api/login');
 });
